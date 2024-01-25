3867d23866b50dbe58ea390f5350f8220627b3ca
evilasioferreira <157721668+evilasioferreira@users.noreply.github.com>
2024-01-25 12:14:18 -0300

    Add files via upload

---
+++ livro/livro_listar.py
@@ -0,0 +1,14 @@
+from repositorio.livro_repositorio import livro_repositorio
+
+def listarLivros() -> None:
+    print("--- TODOS OS LIVROS ---")
+    for livro in livro_repositorio:
+        print(f"Codigo: {livro['codigo']}")
+        print(f"Titulo: {livro['titulo']}")
+        print(f"Autor: {livro['autor']}")
+        print(f"Emprestado? {livro['emprestado']}")
+        print("-" * 20)
+
+if __name__ == "__main__":
+    listarLivros()
+
